87cbd9992c182d47e02139bab30ca09afedb8385
Greg Dubela <greg.dubela@icloud.com>
2018-04-23 16:06:20 -0400

    use 127.0.0.1 for development truffle network

--- truffle.js
+++ truffle.js
@@ -7,7 +7,7 @@ module.exports = {
 
   networks: {
     development: {
-      host: "ganache",//"127.0.0.1",
+      host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
     },
